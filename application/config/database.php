f7284d1e1fc607fbc89e38819696ebaa7e380825
Ling <kelingyuan@gmail.com>
2012-11-16 12:25:36 -0500

    Add godaddy db info

--- application/config/database.php
+++ application/config/database.php
@@ -48,21 +48,49 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'wescourse';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+switch (ENVIRONMENT)
+{
+	case 'development':
+		$db['default']['hostname'] = 'localhost';
+		$db['default']['username'] = 'root';
+		$db['default']['password'] = 'root';
+		$db['default']['database'] = 'wescourse';
+		$db['default']['dbdriver'] = 'mysql';
+		$db['default']['dbprefix'] = '';
+		$db['default']['pconnect'] = TRUE;
+		$db['default']['db_debug'] = TRUE;
+		$db['default']['cache_on'] = FALSE;
+		$db['default']['cachedir'] = '';
+		$db['default']['char_set'] = 'utf8';
+		$db['default']['dbcollat'] = 'utf8_general_ci';
+		$db['default']['swap_pre'] = '';
+		$db['default']['autoinit'] = TRUE;
+		$db['default']['stricton'] = FALSE;
+	break;
+
+	case 'testing':
+	case 'production':
+		$db['default']['hostname'] = 'wescourse.db.10088040.hostedresource.com';
+		$db['default']['username'] = 'wescourse';
+		$db['default']['password'] = 'G0wesleyan!';
+		$db['default']['database'] = 'wescourse';
+		$db['default']['dbdriver'] = 'mysql';
+		$db['default']['dbprefix'] = '';
+		$db['default']['pconnect'] = TRUE;
+		$db['default']['db_debug'] = TRUE;
+		$db['default']['cache_on'] = FALSE;
+		$db['default']['cachedir'] = '';
+		$db['default']['char_set'] = 'utf8';
+		$db['default']['dbcollat'] = 'utf8_general_ci';
+		$db['default']['swap_pre'] = '';
+		$db['default']['autoinit'] = TRUE;
+		$db['default']['stricton'] = FALSE;
+	break;
+
+	default:
+		exit('The application environment is not set correctly.');
+}
+
 
 
 /* End of file database.php */
